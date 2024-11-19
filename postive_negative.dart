80b14267347f4db881d11c6d8fa73e74d00d148f
Misheel31 <misheelrai7@gmail.com>
2024-11-19 16:38:34 +0545

    PositiveandNegative

---
+++ postive_negative.dart
@@ -0,0 +1,9 @@
+void checkNumberSign(int num) {
+  if (num > 0) {
+    print('$num is positive.');
+  } else if (num < 0) {
+    print('$num is negative.');
+  } else {
+    print('The number is zero.');
+  }
+}
